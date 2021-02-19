c0a9ea8f9c72b7f9c5b1f7197d4439d04d9f38b6
Dima Savchenko <savvaq@gmail.com>
2021-02-18 21:32:07 -0500

    working on it

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+    name: 'Light & Spacious Garden Flat Munich',
+    address: '85 Clifton Gardens London W9 1DT',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 55,
+    number_of_guests: 4
+  )
